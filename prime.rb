d4c0c9a566d279b8337be08042f470bdd39a2ba5
minh nguyen <imnotminh@gmail.com>
2019-03-25 22:45:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+# def prime?(integer)
+#   def prime?(n)
+#   (2..Math.sqrt(n)).none? {|f| n % f == 0}
+# end
+# end
+
+def prime?(n)
+  if n<=1
+    return false
+  else
+  (2..Math.sqrt(n)).none?{|x| n%x==0}
+end
+end
